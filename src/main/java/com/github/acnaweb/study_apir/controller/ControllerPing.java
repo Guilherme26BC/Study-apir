e0dde852373d823cc5a67c4ad7527c35bc6bf9ba
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-09 22:11:54 -0300

    feat: add pongx

--- src/main/java/com/github/acnaweb/study_apir/controller/ControllerPing.java
+++ src/main/java/com/github/acnaweb/study_apir/controller/ControllerPing.java
@@ -13,7 +13,7 @@ public class ControllerPing {
     private String version ;
     @GetMapping
     public String ping() {
-        return "pong";
+        return "pongx";
     }
     @GetMapping("/version")
     public String version() {return "Versão: " + version; }
