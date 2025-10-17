4599fa568e34ea29b0956f20e84fdec7b410cdd6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-16 22:10:50 -0300

    refact: add testes

---
+++ src/test/java/com/github/acnaweb/study_apir/DemoTest.java
@@ -0,0 +1,19 @@
+package com.github.acnaweb.study_apir;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DemoTest {
+
+    @DisplayName("test fail")
+    @Test
+    void testGivenNewProduto_whenCreate_thenFail() throws Exception{
+         fail("deu erro O_O ");
+    }
+    @Test
+    void testeNovo() throws Exception{
+
+    }
+}
